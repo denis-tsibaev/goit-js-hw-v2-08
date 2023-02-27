63b15c86c941e42136f5e4618ce937e524f7b103
denis-tsibaev <denis.tsibaev@gmail.com>
2023-02-27 21:40:08 +0700

    Update 03-feedback.js
    Закончил третье задание

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -26,82 +26,46 @@
 // 4. Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд.
 // Для этого добавь в проект и используй библиотеку lodash.throttle.
 
-// const throttle = require('lodash.throttle');
-// const LOCAL_STORAGE_KEY = 'feedback-form-state';
-// const refs = {
-//   form: document.querySelector('.feedback-form'),
-//   input: document.querySelector('.feedback-form input'),
-//   textarea: document.querySelector('.feedback-form textarea'),
-// };
-
-// const formData = {};
-
-// let savedFormData = localStorage.getItem(LOCAL_STORAGE_KEY);
-// const parsedFormData = JSON.parse(savedFormData);
-
-// if (savedFormData) {
-//   parsedFormData.email
-//     ? (refs.input.value = parsedFormData.email)
-//     : (refs.input.value = '');
-
-//   parsedFormData.message
-//     ? (refs.textarea.value = parsedFormData.message)
-//     : (refs.textarea.value = '');
-//   console.log(parsedFormData);
-// }
-
-// refs.form.addEventListener('input', e => {
-//   const name = e.target.name;
-//   const value = e.target.value;
-
-//   formData[name] = value;
-//   console.log(formData);
-//   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
-// });
-
-// refs.form.addEventListener('submit', e => {
-//   e.preventDefault();
-//   e.currentTarget.reset();
-//   localStorage.removeItem(LOCAL_STORAGE_KEY);
-// });
-
-// варик типа, у него  не забывает поля
-
-const LOCAL_STORAGE_KEY = 'feedback-form-state';
 const throttle = require('lodash.throttle');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
-const formEl = document.querySelector('.feedback-form');
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+const savedFormData = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+fillFormFields();
 
 function fillFormFields() {
-  let formInfoLS = localStorage.getItem(LOCAL_STORAGE_KEY);
-
-  if (formInfoLS) {
-    formInfoLS = JSON.parse(formInfoLS);
-    Object.entries(formInfoLS).forEach(([name, value]) => {
-      formEl.elements[name].value = value;
+  if (savedFormData) {
+    console.log('savedFormData: string: ', savedFormData);
+    Object.entries(JSON.parse(savedFormData)).forEach(([name, value]) => {
+      refs.form.elements[name].value = value;
     });
   }
 }
-fillFormFields();
 
-function onInputText(e) {
-  let userInfoLS = localStorage.getItem(LOCAL_STORAGE_KEY);
-  userInfoLS = userInfoLS ? JSON.parse(userInfoLS) : {};
-  userInfoLS[e.target.name] = e.target.value;
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(userInfoLS));
-}
+refs.form.addEventListener('input', throttle(onLocalStorageSetItem, 500));
 
-function onSubmit(e) {
-  e.preventDefault();
-  if (formEl.elements.message.value && formEl.elements.email.value) {
-    console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
-    localStorage.removeItem(LOCAL_STORAGE_KEY);
-    formEl.reset();
+function onLocalStorageSetItem(e) {
+  let formData = {};
+
+  if (savedFormData) {
+    formData = JSON.parse(savedFormData);
   }
+
+  formData[e.target.name] = e.target.value;
+  console.log('formData from input: ', formData);
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
 }
 
-const onInputTextThrottle = throttle(onInputText, 500);
-const onSubmitThrottle = throttle(onSubmit, 500);
+refs.form.addEventListener('submit', onFormSubmit);
 
-formEl.addEventListener('input', onInputTextThrottle);
-formEl.addEventListener('submit', onSubmitThrottle);
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
